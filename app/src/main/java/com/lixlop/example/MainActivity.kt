8d1a489e50356537a4ddf610f150ca22573d9aeb
lixlop <320044933@qq.com>
2019-09-30 07:13:09 +0800

    example

--- app/src/main/java/com/lixlop/example/MainActivity.kt
+++ app/src/main/java/com/lixlop/example/MainActivity.kt
@@ -2,11 +2,24 @@ package com.lixlop.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GlobalScope.launch {
+            val image = async {
+
+            }
+            val name = async {
+
+            }
+            withContext(Dispatchers.IO){
+
+            }
+            image.await()
+        }
     }
 }
---
+++ app/src/main/java/com/lixlop/example/rv/RecyclerViewActivity.kt
@@ -0,0 +1,4 @@
+package com.lixlop.example.rv
+
+class RecyclerViewActivity {
+}
